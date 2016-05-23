e970ed226e6511f7a4df128da718dd6ae872c370
jisonami <jisonami@163.com>
2016-05-23 14:17:23 +0800

    add toString method

--- src/main/java/org/jisonami/entity/User.java
+++ src/main/java/org/jisonami/entity/User.java
@@ -42,5 +42,10 @@ public class User implements Serializable{
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", password=" + password
+				+ "]";
+	}
 	
 }
