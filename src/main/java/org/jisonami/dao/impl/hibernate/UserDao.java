9efdf78337c655c3ca3a139453265a74c15e06f1
jisonami <jisonami@163.com>
2016-06-04 18:51:41 +0800

    add hibernate implements

--- src/main/java/org/jisonami/dao/impl/hibernate/UserDao.java
+++ src/main/java/org/jisonami/dao/impl/hibernate/UserDao.java
@@ -1,8 +1,11 @@
 package org.jisonami.dao.impl.hibernate;
 
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -60,34 +63,59 @@ public class UserDao implements IUserDao {
 		return list;
 	}
 
+	public <T> List<T> query(String hql, Map<String, Object> queryParam) {
+		Session session = initSession(false);
+		Query query = session.createQuery(hql);
+		for (String key : queryParam.keySet()) {
+			query.setParameter(key, queryParam.get(key));
+		}
+		@SuppressWarnings("unchecked")
+		List<T> list = query.list();
+		closeSession(session, false);
+		return list;
+	}
+	
 	@Override
 	public boolean selectByNameAndPassword(User user) throws SQLException {
-		// TODO Auto-generated method stub
+		String hql = "from User as u where u.name = :name and u.password = :password";
+		Map<String, Object> queryParam = new HashMap<String, Object>();
+		queryParam.put("name", user.getName());
+		queryParam.put("password", user.getPassword());
+		List<User> users = this.query(hql, queryParam);
+		if(users!=null && users.size()>0){
+			return true;
+		}
 		return false;
 	}
 
 	@Override
 	public boolean selectByName(String name) throws SQLException {
-		// TODO Auto-generated method stub
+		String hql = "from User as u where u.name = :name";
+		Map<String, Object> queryParam = new HashMap<String, Object>();
+		queryParam.put("name", name);
+		List<User> users = this.query(hql, queryParam);
+		if(users!=null && users.size()>0){
+			return true;
+		}
 		return false;
 	}
 
 	private Session initSession(boolean transactionFlag) {
-	StandardServiceRegistry standardRegistry = new StandardServiceRegistryBuilder()
-			.configure().build();
-	Metadata metadata = new MetadataSources(standardRegistry)
-			.getMetadataBuilder()
-			.applyImplicitNamingStrategy(
-					ImplicitNamingStrategyJpaCompliantImpl.INSTANCE)
-			.build();
-	SessionFactory sessionFactory = metadata.getSessionFactoryBuilder().build();
-	Session session = sessionFactory.openSession();
-
-	if (transactionFlag) {
-		session.beginTransaction();
+		StandardServiceRegistry standardRegistry = new StandardServiceRegistryBuilder()
+				.configure().build();
+		Metadata metadata = new MetadataSources(standardRegistry)
+				.getMetadataBuilder()
+				.applyImplicitNamingStrategy(
+						ImplicitNamingStrategyJpaCompliantImpl.INSTANCE)
+				.build();
+		SessionFactory sessionFactory = metadata.getSessionFactoryBuilder().build();
+		Session session = sessionFactory.openSession();
+	
+		if (transactionFlag) {
+			session.beginTransaction();
+		}
+		return session;
 	}
-	return session;
-}
 
 	private boolean closeSession(Session session, boolean transactionFlag) {
 		if (transactionFlag) {
