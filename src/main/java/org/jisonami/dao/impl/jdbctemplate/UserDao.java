2996e5d2a4a00a27248d024c1903b72ae857f6ab
jisonami <jisonami@163.com>
2016-06-06 07:30:51 +0800

    jdbc template

--- src/main/java/org/jisonami/dao/impl/jdbctemplate/UserDao.java
+++ src/main/java/org/jisonami/dao/impl/jdbctemplate/UserDao.java
@@ -7,6 +7,7 @@ import org.apache.commons.dbcp.BasicDataSource;
 import org.jisonami.dao.IUserDao;
 import org.jisonami.entity.User;
 import org.jisonami.util.DBUtils;
+import org.jisonami.util.StringUtils;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 public class UserDao implements IUserDao {
@@ -23,8 +24,19 @@ public class UserDao implements IUserDao {
 	}
 	
 	@Override
-	public boolean insert(User t) throws SQLException {
-//		jdbcTemplate.update(sql, pss)
+	public boolean insert(User user) throws SQLException {
+		user.setId(StringUtils.generateUUID());
+		String sql = "insert into t_user(id, name, password) values(?, ?, ?)";
+		
+		int rowChange = jdbcTemplate.update(sql, (preStmt) -> {
+			preStmt.setString(1, user.getId());
+			preStmt.setString(2, user.getName());
+			preStmt.setString(3, user.getPassword());
+		});
+		
+		if(rowChange != 0){
+			return true;
+		}
 		return false;
 	}
 
@@ -35,7 +47,7 @@ public class UserDao implements IUserDao {
 	}
 
 	@Override
-	public boolean update(User t) throws SQLException {
+	public boolean update(User user) throws SQLException {
 		// TODO Auto-generated method stub
 		return false;
 	}
@@ -54,7 +66,21 @@ public class UserDao implements IUserDao {
 
 	@Override
 	public boolean selectByNameAndPassword(User user) throws SQLException {
-		// TODO Auto-generated method stub
+		String sql = "select name,password from t_user t where t.name = ? and t.password = ?";
+		User result = (User) jdbcTemplate.queryForObject(sql, new Object[]{
+				user.getName(),
+				user.getPassword()
+				}, 
+				(rs, rowNum) -> {
+					User u = new User();
+					u.setId(rs.getString("id"));
+					u.setName(rs.getString("name"));
+					u.setPassword(rs.getString("password"));
+					return u;}
+		);
+		if(result!=null){
+			return true;
+		}
 		return false;
 	}
 
